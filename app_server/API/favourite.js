17137258951987ef374550e62cac266a393915a9
Uche <uche.anota@yahoo.com>
2018-01-09 01:03:21 +0100

    new changes commited

--- app_server/API/favourite.js
+++ app_server/API/favourite.js
@@ -1,20 +1,20 @@
 // Use array push or pop to remove or add a player or a team respectively to a user.
 //If user is not a logged in before trying to add a favourite, system prompts for login/sign up
 //creates an array of empty favourite bucket for a user
-//Create an object for players and clubs and add to them.
 //use set timeout function to create image sliders for main content
 
-let mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
-let userFavouriteSchema = mongoose.Schema({
+var favourite = mongoose.Schema({
     userId: Number,
     club: String,
     player: String,
     matches: String,
+    email: String,
     date: Date,
     updateId: String,
     approved: Boolean,
 });
 
-let Favourite = mongoose.model('Favourite', userFavouriteSchema);
+var Favourite = mongoose.model('Favourite', favourite);
 module.exports = Favourite;
